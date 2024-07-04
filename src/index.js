84caa083a8e4a6873899f687680f318daa93f314
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-07-04 14:26:21 -0300

    alterações mais recentes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 const express = require('express')
+const { Pool } = require("pg")
 
 const app = express()
+app.use(express.json())
+
+
+/**
+ * app.use('/caminho', xyzRoutes)
+ */
+
+const conexao = new Pool({
+    host: "localhost",
+    port: "5432",
+    user: "postgres",
+    password: "1234",
+    database: "lab_commerce",
+  });
+  
+
+app.listen(3000, () => {
+    console.log("Servidor OnLine!");
+  });
