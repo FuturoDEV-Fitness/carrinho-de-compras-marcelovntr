5842fa44c3be3b6694caf3e5ea368f613309d9ae
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-07-05 20:03:04 -0300

    cadastrar produto funcional e adequado

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,31 +1,50 @@
-const {Pool} = require("pg")
+const { Pool } = require("pg");
 
 const conexao = new Pool({
-    host: "localhost",
-    port: "5432",
-    user: "postgres",
-    password: "1234",
-    database: "lab_commerce",
-  });
+  host: "localhost",
+  port: "5432",
+  user: "postgres",
+  password: "1234",
+  database: "lab_commerce",
+});
 
-  class ProductController{
+class ProductController {
+  async criar(request, response) {
+    try {
+      const dados = request.body;
 
-    async criar(request, response){
-        const dados = request.body
-        
+      if (!dados.name || !dados.category_id) {
+        return response.json({
+          mensagem: "nome e identificador da categoria são obrigatórios!",
+        });
+      }
+
+     const produtoInserido = await conexao.query(
+        `INSERT INTO products (name, amount, color, voltage, description, category_id)
+        VALUES($1, $2, $3, $4, $5, $6) returning*`,
+        [
+          dados.name,
+          dados.amount,
+          dados.color,
+          dados.voltage,
+          dados.description,
+          dados.category_id
+        ]
+      );
+      response.status(201).json(produtoInserido.rows[0])
+    } catch (error) {
+      response
+      .status(400)
+      .json({ mensagem: "Erro inesperado ao cadastrar produto!" });
     }
-
-    async listar(request, response){
-
-    }
-
-    async atualizar(request, response){
-
-    }
-
-    async apagar(request, response){
-      
-    }
+   
   }
 
-  module.exports = new ProductController()
+  async listar(request, response) {}
+
+  async atualizar(request, response) {}
+
+  async apagar(request, response) {}
+}
+
+module.exports = new ProductController();
