e400ccf25451694221149604a705875bd7a0cf8e
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-07-08 17:48:05 -0300

    versão final do projeto 1.0

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
+Carrinho de Compras
+
+● Função
+
+O prejeto permite a simulação de ações na interação de uma interface com banco de dados. É possível cadastrar clientes, categorias de produtos e produtos com suas características. Com os dados disponíveis referentes a esses, é possível simular a requisições de compras com listas de produtos, assim armazenando os dados dos pedidos no banco, além de gravar os pedidos feitos com seus itens para posteriores consultas.
+;
+
+● Tecnologias utilizadas JavaScript, Node (Nodemon), Express, PostMan, PostgreSQL (pgAdmin), ;
+
+● Como executar:
+
+Obter o repositório utilizando
+
+    git pull https://github.com/FuturoDEV-Fitness/carrinho-de-compras-marcelovntr;
+
+Executar os seguintes comandos para instalar dependências de demais elementos:
+
+    npm init -y;
+    npm install Nodemon --save-dev;
+    npm install express;
+    npm install pg;
+
+
+
+● Melhorias cabíveis:
+
+    Introduzir no projeto uma classe que estabeleça a conexão com banco de dados, assim reutilizando o código por meio de importação;
+    Revisão de validações do corpo das requisições feitas para o banco de dados, a exemplo do preço dos produtos;
+    
+
+Vídeo explicativo:
+
+    https://drive.google.com/drive/u/0/folders/13zM___VB1K37QfbhuhZY9XL1geeUWfTQ *Errata: Quando se fala em não completude da função de cadastro, refere-se a edição de cadastro do usuário, função não obrigatória do aplicativo.
+
+
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/dNOfMvCD)
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-2e0aaae1b6195c2367325f4f02e2d04e9abb55f0b24a779b69b11b9e10269abc.svg)](https://classroom.github.com/online_ide?assignment_repo_id=15363411&assignment_repo_type=AssignmentRepo)
--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -10,51 +10,72 @@ const conexao = new Pool({
 
 class OrderController {
   async criar(request, response) {
-
     try {
       const dados = request.body;
- 
-      const precos = await Promise.all(dados.products.map(async (item) => {
-        const produtoAtual = await conexao.query(
+
+      if(!dados.client_id || !dados.address){
+return response.json({mensagem: "O ID do cliente e o endereço são obrigatórios"})
+      }
+      if(typeof dados.client_id !== "number" || !Number.isInteger(dados.client_id)){
+        return response.json({mensagem:"o ID precisa ser um número inteiro!"})
+      }
+
+      //Promise.all para permitir a ordem das ações no código, mais especificamente o "map"
+      const precos = await Promise.all(
+        dados.products.map(async (item) => {
+          const produtoAtual = await conexao.query(
             `SELECT price FROM products where id = $1`,
-            [item.product_id] 
-        );
-        return produtoAtual.rows[0].price * item.amount;
-    }));
+            [item.product_id]
+          );
+          return produtoAtual.rows[0].price * item.amount;
+        })
+      );
 
-    const total = precos.reduce((acc, atual) => acc + atual, 0);
+      //reduce para calcular o total do price somado dos products
+      const total = precos.reduce((acc, atual) => acc + atual, 0);
 
+      if(!total){
+        return response.json({mensagem: "Houve erro ao obter o preço total dos produtos!"})
+      }
 
-
+      //EFETUANDO PEDIDO
       const pedidoEfetuado = await conexao.query(
         `INSERT INTO orders (client_id, address, observations, total)
       VALUES($1, $2, $3, $4) returning*`,
         [dados.client_id, dados.address, dados.observations, total] //< mudar p/ valor genérico para testar
       );
 
-  await Promise.all(dados.products.map(async (item) => {
-    const produtoAtual = await conexao.query(
-      `SELECT price FROM products WHERE id = $1`,
-      [item.product_id]
-    );
-      await conexao.query(
-          `INSERT INTO orders_items (orders_id, products_id, amount, price)
-  VALUES($1, $2, $3, $4) returning*`,
-          [
-            pedidoEfetuado.rows[0].id,
-            item.product_id,
-            item.amount,
-            produtoAtual.rows[0].price, //MUDAR PRA TOTAL!!! e mudar para pedidoEfetuado.total OU só total(?)
-          ]
-        );
-      
-    } ))
-    
-    response.status(201).json({mensagem: "Pedido realizado!", total: pedidoEfetuado.rows[0].total})
-  }catch (error) {
-      response.status(400).json({mensagem: "Erro ao criar pedido!"})
-    }
 
+      await Promise.all(
+        dados.products.map(async (item) => {
+          const produtoAtual = await conexao.query(
+            `SELECT price FROM products WHERE id = $1`,
+            [item.product_id]
+          );
+
+          //INSERINDO ITEMS DO PEDIDO
+          await conexao.query(
+            `INSERT INTO orders_items (orders_id, products_id, amount, price)
+  VALUES($1, $2, $3, $4) returning*`,
+            [
+              pedidoEfetuado.rows[0].id,
+              item.product_id,
+              item.amount,
+              produtoAtual.rows[0].price, //MUDAR PRA TOTAL!!! e mudar para pedidoEfetuado.total OU só total(?)
+            ]
+          );
+        })
+      );
+
+      response
+        .status(201)
+        .json({
+          mensagem: "Pedido realizado!",
+          total: pedidoEfetuado.rows[0].total,
+        });
+    } catch (error) {
+      response.status(400).json({ mensagem: "Erro ao criar pedido!" });
+    }
   }
 }
 
